12fd47342782c11288e44389218de89d3a3e4b54
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-10-09 21:04:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -15,3 +15,5 @@ def update_counting_hash(hash, key)
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
 end
+
+read_from_hash({:name => "Mitch"}, name)
